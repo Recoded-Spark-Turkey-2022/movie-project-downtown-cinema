3e6ec3e8a5cf33969560a85494baa2b4b37da87f
trteel1 <tateelamino@gmail.com>
2022-11-13 23:03:50 +0300

    connecting the buttons so they can be scrolled one by one

--- script.js
+++ script.js
@@ -138,4 +138,20 @@ document.addEventListener("DOMContentLoaded", ()=>{
   autorun4()
 });
 
+var slideIndex = 1;
+showDivs(slideIndex);
 
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("movie");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
