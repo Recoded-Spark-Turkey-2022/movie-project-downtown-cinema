4061ec5e46d6f3652b4e5c91cf6052b83184fc74
trteel1 <tateelamino@gmail.com>
2022-11-13 23:52:25 +0300

    fixing some mistakes

--- script.js
+++ script.js
@@ -146,10 +146,10 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("movie");
+  var x = document.getElementsByClassName("homeMidContainer");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
+  for (i = 0; i < x.length; i+=5) {
     x[i].style.display = "none";
   }
   x[slideIndex-1].style.display = "block";
