d409b5ee76e2803f795f69222567272bb41dd2cc
trteel1 <tateelamino@gmail.com>
2022-11-14 18:41:02 +0300

    the last try for this code

--- script.js
+++ script.js
@@ -146,10 +146,10 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("homeMidContainer");
+  var x = document.getElementsByClassName("movie");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i+=5) {
+  for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
   x[slideIndex-1].style.display = "block";
